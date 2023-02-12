d96f1ab98bd6fcf0110dd84a1e05076f9f462701
Naga Nitish <95007559+NagaNitishChowdary@users.noreply.github.com>
2023-02-12 12:39:50 +0530

    Create 1)5) FIVE_NUMBER_SUMMARY_OF_DATA.C

---
+++ 1)5) FIVE_NUMBER_SUMMARY_OF_DATA.C
@@ -0,0 +1,23 @@
+// 1)D) GIVE THE FIVE NUMBER SUMMARY OF DATA 
+// FIVE NUMBER SUMMARY ( MIN NO , FIRST QUARTILE , MEDIAN , THIRD QUARTILE , MAX NO)
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(){
+    printf("ENTER NO OF VALUES\n") ;
+    int n ;
+    scanf("%d" , &n) ;
+    printf("enter %d values\n",n) ;
+    int i , arr[n] ;
+    for(i=0 ; i<n ; i++) scanf("%d",&arr[i]) ;  
+
+    double q1 = arr[(n+1)/4] ;
+    double q3 = arr[3*(n+1)/4] ;
+    double iqr = q3 - q1 ;
+    printf("THE 5 NUMBER SUMMARY OF DATA IS :\n") ;
+    printf("%d  %.2f  %.2f  %.2f  %d\n",arr[0],q1,iqr,q3,arr[n-1]) ;
+
+    return 0 ;
+}
